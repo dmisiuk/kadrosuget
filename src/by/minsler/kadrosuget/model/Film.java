f1ba1f7adb8c0e96dd65704e276b268eb550701c
minsler <minsler.by@gmail.com>
2012-11-04 21:41:43 +0300

    Added models and gson lib

---
+++ src/by/minsler/kadrosuget/model/Film.java
@@ -0,0 +1,12 @@
+package by.minsler.kadrosuget.model;
+
+/**
+ * User: dzmitry.misiuk
+ * Date: 11/4/12
+ * Time: 9:37 PM
+ */
+public class Film {
+
+    private Trailer trailer;
+    private Kadr[] kadrosuget;
+}
---
+++ src/by/minsler/kadrosuget/model/Kadr.java
@@ -0,0 +1,12 @@
+package by.minsler.kadrosuget.model;
+
+/**
+ * User: dzmitry.misiuk
+ * Date: 11/4/12
+ * Time: 9:37 PM
+ */
+public class Kadr {
+
+    private String url;
+    private String text;
+}
